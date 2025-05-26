b5225ecaab80744ace5bb0ab1f8b2cc7e65c30cb
Dafydd Jenkins <dafydd.a.jenkins@gmail.com>
2025-05-25 22:00:02 -0300

    cancha finished

--- servidor/src/services/cancha.service.js
+++ servidor/src/services/cancha.service.js
@@ -1,7 +1,22 @@
-class CanchaService {
-    getCanchas(queryParams) {
-      return { message: 'List of canchas', filters: queryParams };
-    }
-  }
+const canchaRepository = require('../repositories/cancha.repository');
 
-  module.exports = new CanchaService();
+class CanchaService {
+  getCanchas(queryParams) {
+        
+    const allCanchas = canchaRepository.getCanchas();
+
+    console.log('All canchas:', allCanchas);
+
+    const filteredCanchas = allCanchas.filter(cancha => {
+      return Object.keys(queryParams).every(key => {
+        return cancha[key] && cancha[key].toString() === queryParams[key].toString();
+      });
+    });
+
+    return { message: 'List of canchas', data: filteredCanchas };
+  }
+}
+  
+module.exports = new CanchaService();
+
+
